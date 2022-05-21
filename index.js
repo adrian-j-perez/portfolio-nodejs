d4805ca5370929931c9a3daf6b2da80cce3a916f
Adrian <adrianjperez@lewisu.edu>
2022-05-21 17:58:57 -0500

    add detail and images to projects tab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// https://www.geeksforgeeks.org/handlebars-templating-in-expressjs/
+// By Adrian Perez
 
 
 const express = require('express')
@@ -8,6 +8,8 @@ const app = express()
 //static files or try app.use(express.static(__dirname + '/public'))
 app.use(express.static('public'))
 
+// webpage routes in another js file//
+require('./routes')(app)
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
@@ -18,44 +20,11 @@ app.set("views", './views')
 
 const port = process.env.PORT || 3000;
 
-
-// webpage routes // - should make a diffenr js file just for webroute for a clean look 
-app.get('/', (req, res) => {
-      //res.sendFile(__dirname + '/views/newmain.html')
-    res.render('homepage')
-})
-
-app.get('/projects', (req, res) => {
-    //res.sendFile(__dirname + '/views/newmain.html')
-  res.render('projects')
-})
-
-app.get('/about', (req, res) => {
-    //res.render('./public/projects/know-each-other/adrian-webpage')
-    // res.sendFile(__dirname + '/public/projects/know-each-other/adrian-webpage.html')
-    res.render("about")
-})
-
-app.get('/resume', (req, res) => {
-    res.render('resume')
-})
-
-app.get('/reg-exp-tester', (req, res) => {
-    //res.render('./public/projects/know-each-other/adrian-webpage')
-    res.sendFile(__dirname + '/public/projects/reg-exp-tester/regexp.html')
-})
-
-app.get('/dice-roller', (req, res) => {
-    res.sendFile(__dirname + '/public/projects/dice-roller/index.html')
-})
-
-
 /*
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 */
-
 
 app.use((req, res) => {
     res.status(404)
@@ -72,3 +41,4 @@ app.use((req, res) => {
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
     `press Ctrl-C to terminate.`))
+    
